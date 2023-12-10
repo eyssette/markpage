bf6568b83f5e204d24c90a5552aaa7d84c98922c
Eyssette <cedric.eyssette@gmail.com>
2023-12-11 00:43:38 +0100

    displayResults: HTML+JS

--- markpageHandle.js
+++ markpageHandle.js
@@ -2,6 +2,7 @@ function handleMarkpage() {
 
 	const bodyElement = document.body;	
 
+	const sectionsTitle = markpageData[2];
 	const subSectionsData = markpageData[3];
 
 	function convertLatexExpressions(string) {
@@ -169,5 +170,64 @@ function handleMarkpage() {
 		changeDisplayBasedOnParams(params);
 	});
 	
-	console.log(markpageData)
+	// console.log(markpageData)
+
+
+	function searchText() {
+		let sectionsResults = [];
+		let subSectionsResults = [];
+		const inputText = document.getElementById("searchInput").value.toLowerCase();
+		if (inputText.length>2) {
+			for (let i = 0; i < subSectionsData.length; i++) {
+				// Recherche dans le titre de chaque section + le contenu de chaque section
+				const textSection = sectionsTitle[i].toString().toLowerCase()+subSectionsData[i].toString().toLowerCase();
+				if (textSection.indexOf(inputText)>-1) {
+					// On a trouvé le texte dans la section
+					if (subSectionsData[i].length>1) {
+						// S'il y a des sous-sections, on affine la recherche dans chaque sous-section
+						for (let j = 0; j < subSectionsData[i].length; j++) {
+							// recherche du texte dans le contenu de chaque sous-section
+							const textSubSection = subSectionsData[i][j].toString().toLowerCase();
+							if (textSubSection.indexOf(inputText)>-1) {
+								subSectionsResults.push([i,j])
+							}
+						}
+					} else {
+						// Sinon, on indique juste comme résultat la section correspondante
+						sectionsResults.push(i)
+					}
+				}
+			}
+		}
+		const numberResultsSection = sectionsResults.length
+		const numberResultsSubSection = subSectionsResults.length
+		const numberResults = numberResultsSection + numberResultsSubSection;
+		const displayResultsElement = document.getElementById('displayResults');
+		let displayResultsHTML = '';
+		if (numberResults>0) {
+			displayResultsHTML = '<ul>'
+			if(numberResultsSection>0) {
+				for (const indexSection of sectionsResults) {
+					displayResultsHTML += '<li><a href="?sec='+(indexSection+1)+'#'+hash+'">'+sectionsTitle[indexSection]+'</a></li>'		
+				}
+			}
+			if(numberResultsSubSection>0) {
+				for (const result of subSectionsResults) {
+					const indexSection = result[0];
+					const indexSubSection = result[1];
+					const subSectionElement = document.getElementById('subSection-'+(indexSubSection+1))
+					const subSectionTitle = subSectionElement.querySelector('h3 a').innerHTML
+					displayResultsHTML += '<li><a href="?sec='+(indexSection+1)+'&subsec='+(indexSubSection+1)+'#'+hash+'">'+subSectionTitle+'</a></li>'		
+				}
+			}
+			displayResultsHTML +='</ul>'
+		}
+		displayResultsElement.innerHTML = displayResultsHTML;
+
+		
+	
+	}
+
+	document.getElementById("searchInput").addEventListener("input", searchText);
+
 };
