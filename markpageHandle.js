ce755be8333dab8b81f539c26ae8168537e9e0b9
Eyssette <cedric.eyssette@gmail.com>
2024-01-18 00:02:46 +0100

    quand ?h3=0, ajouter contenu titre h3 en haut de la page

--- markpageHandle.js
+++ markpageHandle.js
@@ -192,11 +192,21 @@ function handleMarkpage() {
 			linkToHomePageElement.style.display = "none"
 		}
 	}
+	// On peut enlever les titres h3 sur le côté
 	if (params.h3 && params.h3 == 0) {
 		const h3Elements = document.querySelectorAll('h3')
 		for (const h3Element of h3Elements) {
 			h3Element.style.display="none";
 		}
+		if (params.subsec) {
+			// À la place du titre en haut de page, on met le titre h3 de la section affichée
+			const h3Title = h3Elements[params.subsec-1].firstChild.innerHTML
+			if (params.sec && h3Title) {
+				const h2Title = document.querySelector('#section-'+params.sec+' h2 a')
+				h2Title.parentElement.innerHTML = h2Title.innerHTML + ' / ' + h3Title; 
+			}
+		}
+		// On recale correctement en CSS le contenu de la section, étant donné qu'on a supprimé la partie "titres h3" sur la gauche
 		const styleSubSectionContent = '@media screen and (min-width: 601px) {.subSectionContent {margin-left:140px}}';
 		const styleSheet = document.createElement("style");
 		styleSheet.innerText = styleSubSectionContent;
