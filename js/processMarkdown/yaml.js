f2bc33a6826aae223f5723d85a08372f5e675aa8
eyssette <cedric.eyssette@gmail.com>
2024-10-11 09:48:37 +0200

    function setTheme

--- js/processMarkdown/yaml.js
+++ js/processMarkdown/yaml.js
@@ -1,6 +1,7 @@
 import { load as loadYAML } from "../externals/js-yaml.js";
 import { deepMerge, loadScript, loadCSS } from "../utils.js";
 import { CSSthemes } from "../config.js";
+import { setTheme } from "../ui/setTheme.js";
 
 export let yaml = {
 	searchbar: true,
@@ -9,7 +10,6 @@ export let yaml = {
 };
 
 export function processYAML(markdownContent) {
-	const styleThemeElement = document.getElementById("styleTheme");
 	// Gestion de l'en-tête YAML
 	try {
 		if (
@@ -35,37 +35,9 @@ export function processYAML(markdownContent) {
 			]);
 		}
 		// Gestion des styles personnalisés
+		const styleThemeElement = document.getElementById("styleTheme");
 		if (yaml.theme) {
-			// Possibilité d'utiliser un thème pour les cartes
-			const themeName = yaml.theme.trim();
-			const CSSfile = themeName.endsWith(".css")
-				? themeName
-				: themeName + ".css";
-			if (CSSthemes.includes(CSSfile)) {
-				// theme = true;
-				let themeURL = "css/theme/" + CSSfile;
-				fetch(themeURL)
-					.then((response) => response.text())
-					.then((data) => {
-						styleThemeElement.textContent = data;
-						document.body.className =
-							document.body.className + " theme-" + CSSfile.replace(".css", "");
-					})
-					.catch((error) => {
-						styleThemeElement.textContent = "";
-						document.body.className = document.body.className.replace(
-							/theme-.*/,
-							"",
-						);
-						console.error(error);
-					});
-			} else {
-				styleThemeElement.textContent = "";
-				document.body.className = document.body.className.replace(
-					/theme-.*/,
-					"",
-				);
-			}
+			setTheme(yaml.theme, CSSthemes, styleThemeElement);
 		} else {
 			styleThemeElement.textContent = "";
 			document.body.className = document.body.className.replace(/theme-.*/, "");
---
+++ js/ui/setTheme.js
@@ -0,0 +1,24 @@
+export function setTheme(theme, allowedThemes, styleThemeElement) {
+	// Possibilité d'utiliser un thème pour les cartes
+	const themeName = theme.trim();
+	const CSSfile = themeName.endsWith(".css") ? themeName : themeName + ".css";
+	if (allowedThemes.includes(CSSfile)) {
+		// theme = true;
+		let themeURL = "css/theme/" + CSSfile;
+		fetch(themeURL)
+			.then((response) => response.text())
+			.then((data) => {
+				styleThemeElement.textContent = data;
+				document.body.className =
+					document.body.className + " theme-" + CSSfile.replace(".css", "");
+			})
+			.catch((error) => {
+				styleThemeElement.textContent = "";
+				document.body.className = document.body.className.replace(
+					/theme-.*/,
+					"",
+				);
+				console.error(error);
+			});
+	}
+}
