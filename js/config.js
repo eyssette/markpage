d967eaa836cbb48d64c476605d34b7d9da4c7269
Eyssette <cedric.eyssette@gmail.com>
2024-12-15 19:13:16 +0100

    déplacement dans dossier addOn des scripts addOns

--- js/config.js
+++ js/config.js
@@ -17,13 +17,13 @@ export const CSSthemes = ["colors.css"];
 // Gestion des add-ons
 export const allowedAddOns = {
 	pako: { js: "js/externals/pako.min.js" },
-	kroki: { js: "js/externals/kroki.js" },
+	kroki: { js: "js/addOn/kroki.js" },
 	lightbox: {
-		js: "js/externals/lightbox.js",
+		js: "js/addOn/lightbox.js",
 		css: "<style>img{cursor: pointer}</style>",
 	},
 	text2quiz: {
-		js: "js/externals/text2quiz.js",
+		js: "js/addOn/text2quiz.js",
 		css: "<style>.iframeText2quiz{width:700px; height:750px; max-width: 100%; border:none} @media screen and (max-width: 834px){.iframeText2quiz{height:65vh;}}</style>",
 	},
 	copycode: {
