f94984d63da08f710c0d832da009f9845afb11a4
Eyssette <cedric.eyssette@gmail.com>
2025-03-09 18:23:12 +0100

    fix(markpage): setInterval pour tester window.katex avant  convertLatex

--- js/ui/displayMaths.js
+++ js/ui/displayMaths.js
@@ -1,20 +1,26 @@
 import { convertLatexExpressions } from "../processMarkdown/convertLatex";
 
 export function displayMaths() {
-	setTimeout(() => {
-		const initialMessageElement = document.getElementById("initialMessage");
-		initialMessageElement.innerHTML = convertLatexExpressions(
-			initialMessageElement.innerHTML,
-		);
-		const sectionContent = document.querySelectorAll(".sectionContent");
-		for (const section of sectionContent) {
-			const subSectionsContent = section.querySelectorAll(".subSectionContent");
-			if (subSectionsContent.length > 0) {
-				for (const subSection of subSectionsContent) {
-					subSection.innerHTML = convertLatexExpressions(subSection.innerHTML);
+	const interval = setInterval(() => {
+		if (window.katex) {
+			clearInterval(interval);
+			const initialMessageElement = document.getElementById("initialMessage");
+			initialMessageElement.innerHTML = convertLatexExpressions(
+				initialMessageElement.innerHTML,
+			);
+			const sectionContent = document.querySelectorAll(".sectionContent");
+			for (const section of sectionContent) {
+				const subSectionsContent =
+					section.querySelectorAll(".subSectionContent");
+				if (subSectionsContent.length > 0) {
+					for (const subSection of subSectionsContent) {
+						subSection.innerHTML = convertLatexExpressions(
+							subSection.innerHTML,
+						);
+					}
+				} else {
+					section.innerHTML = convertLatexExpressions(section.innerHTML);
 				}
-			} else {
-				section.innerHTML = convertLatexExpressions(section.innerHTML);
 			}
 		}
 	}, 200);
