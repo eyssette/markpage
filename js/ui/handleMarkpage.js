88e426cf1bbba5af2a83913424e8217c375a6ef9
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-06-03 23:46:16 +0200

    fix: adjustHeight amélioré si bandeau + petit écran

--- js/ui/handleMarkpage.js
+++ js/ui/handleMarkpage.js
@@ -26,7 +26,11 @@ function adjustHeight(element, options) {
 		}
 		// On a au minimum une hauteur de 400px pour chaque colonne (nécessaire si on utilise un bandeau avec Lightpad sur petit écran et que le bandeau est long : sinon la colonne ne s'afficherait que dans l'espace restant, qui serait tout petit)
 		availableHeight = Math.max(availableHeight, 400);
-		availableHeight = yaml.bandeau ? availableHeight - 100 : availableHeight;
+		if (yaml && yaml.bandeau) {
+			availableHeight = window.matchMedia("(min-width: 500px)").matches
+				? availableHeight - 100
+				: availableHeight - 10;
+		}
 		if (options && options.isSubSection) {
 			element.style.maxHeight = `${availableHeight}px`;
 		} else {
