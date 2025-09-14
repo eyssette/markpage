7f02ccc6e1aa2ad9e48ff9456d1139f8fade45dd
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-09-14 23:45:29 +0200

    fix: addOn titleLinks - précision du textFragment avec un préfixe et un suffixe (contenu avant et après le titre)

--- js/addOn/titleLinks.js
+++ js/addOn/titleLinks.js
@@ -6,10 +6,30 @@ function titleLinks() {
 		header.addEventListener("click", async () => {
 			const baseUrl = window.location.href;
 			const baseUrlWithHash = baseUrl.includes("#") ? baseUrl : baseUrl + "#";
-			const title = header.innerText.trim();
-			const encoded = encodeURIComponent(title);
+			const title = encodeURIComponent(header.innerText.trim());
+
+			// On précise le lien en récupérant le contenu avant et après le titre
+			let prefix = "";
+			let prevNode = header.previousSibling.previousSibling;
+			if (prevNode) {
+				const text = prevNode.textContent.trimEnd();
+				const words = text.split(/\s+/);
+				const lastWords = words.slice(-3).join(" ").trim();
+				prefix = lastWords ? encodeURIComponent(lastWords) + "-," : "";
+			}
+			let suffix = "";
+			let nextNode = header.nextSibling.nextSibling;
+			if (nextNode) {
+				const text = nextNode.textContent.trimEnd();
+				const words = text.split(/\s+/);
+				const firstWords = words.slice(0, 3).join(" ").trim();
+				suffix = firstWords ? ",-" + encodeURIComponent(firstWords) : "";
+			}
+
+			const fragment = `${prefix}${title}${suffix}`;
+
 			// Lien vers le titre avec un "text fragment"
-			const fullUrl = `${baseUrlWithHash}:~:text=${encoded}`;
+			const fullUrl = `${baseUrlWithHash}:~:text=${fragment}`;
 			try {
 				await navigator.clipboard.writeText(fullUrl);
 				header.setAttribute("data-copied", "true");
