b61ef20301a01eb946470dd291451c09f62b5828
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-09-28 18:19:07 +0200

    fix: suppression  du paramètre "editor" dans l'URL. Permet de sortir du mode édition si on y est arrivé via une URL contenant le paramètre "editor"

--- js/ui/editor/dom/createCloseButton.js
+++ js/ui/editor/dom/createCloseButton.js
@@ -12,6 +12,17 @@ export function createCloseButton(editorWrapper, jar) {
 				"Vos modifications seront perdues si vous n'avez pas copié le contenu.",
 		);
 		if (confirmClose) {
+			// Suppression du paramètre "editor" dans l'URL s'il existe
+			// (permet de sortir du mode édition si on y est arrivé via une URL contenant le paramètre "editor")
+			const urlParams = new URLSearchParams(window.location.search);
+			if (urlParams.has("editor")) {
+				urlParams.delete("editor");
+				window.history.replaceState(
+					{},
+					"",
+					`${window.location.pathname}?${urlParams}`,
+				);
+			}
 			resetEditor(jar, editorWrapper, button);
 		}
 	});
