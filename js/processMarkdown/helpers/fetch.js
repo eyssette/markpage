e56827440d7426a87b6e1ce3d27b390bf53d9dc0
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2026-05-29 12:56:45 +0200

    fix: pour les fichiers Docs, ajout du titre du document comme titre de niveau H1 dans le contenu s'il n'y en a pas déjà

--- js/processMarkdown/helpers/fetch.js
+++ js/processMarkdown/helpers/fetch.js
@@ -13,7 +13,13 @@ export async function fetchSource(source) {
 		// Cas particulier des fichiers hébergés sur Docs de La Suite numérique
 		// Le contenu est un JSON qu'il faut traiter pour en extraire le Markdown
 		const content = await response.json().then((data) => {
-			const markdownContent = `${data.content}`.replaceAll("***", "---");
+			let markdownContent = `${data.content}`.replaceAll("***", "---");
+			const hasH1titleRegex = /^#\s+(.+)$/m;
+			const hasH1titleInMarkdownContent = hasH1titleRegex.test(markdownContent);
+			if (!hasH1titleInMarkdownContent && data.title) {
+				// Si le Markdown ne contient pas de titre H1, on en ajoute un à partir du titre du document
+				markdownContent = `# ${data.title}\n\n${markdownContent}`;
+			}
 			return markdownContent;
 		});
 		return content;
